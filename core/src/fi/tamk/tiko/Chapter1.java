5b46627250f9643fae354ef4b39a7e214faf844e
Juuso <juuso.toivanen@cs.tamk.fi>
2019-03-07 11:01:03 +0200

    Add: Chapter1 Class

---
+++ core/src/fi/tamk/tiko/Chapter1.java
@@ -0,0 +1,13 @@
+package fi.tamk.tiko;
+
+public class Chapter1 {
+	final int id = 1;
+	final boolean choice = false;
+	final int affect_id = 0;
+	final int affect_steps = 0;
+	final String story = "lorem ipsum ja silleen.";
+	final int numberOfPages = 1;
+	final String choiceBox1 = "none";
+	final String choiceBox2 = "none";
+	final int stepCount = 500;
+}
